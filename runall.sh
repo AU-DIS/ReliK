97830f5e8026f9d0af7bada308d6b8946034a570
CalculassTakeo <max+github@egger-dopplinger.com>
2023-05-02 16:42:26 +0200

    small bash script change

--- runall.sh
+++ runall.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-python approach/experiment_controller.py -e TransE -d $1 -st &
-sleep 10
-python approach/experiment_controller.py -e DistMult -d $1 -st &
-python approach/experiment_controller.py -e PairRE -d $1 -st &
-python approach/experiment_controller.py -e RotatE -d $1 -st &
+python approach/experiment_controller.py -e TransE -d $1 -st
+python approach/experiment_controller.py -e DistMult -d $1 -st 
+python approach/experiment_controller.py -e PairRE -d $1 -st
+python approach/experiment_controller.py -e RotatE -d $1 -st
