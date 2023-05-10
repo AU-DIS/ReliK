f1ae4c65ba23cf217ccad7edbfef334bbbe5d290
CalculassTakeo <max+github@egger-dopplinger.com>
2023-05-10 08:46:51 +0200

    fix in experiment starting system

--- runall.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-python approach/experiment_controller.py -e TransE -d $1 -st
-python approach/experiment_controller.py -e DistMult -d $1 -st 
-python approach/experiment_controller.py -e PairRE -d $1 -st
-python approach/experiment_controller.py -e RotatE -d $1 -st
